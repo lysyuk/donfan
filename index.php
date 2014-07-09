8784d1a802d68bd22da7ddec02bcc4adf1ca4558
Yury Lysyuk <lysyuk.y@gmail.com>
2014-07-09 11:47:16 +0600

    Add webvisor

--- index.php
+++ index.php
@@ -728,32 +728,33 @@
 		<script src="js/main.js"></script>
 		<!-- Yandex.Metrika counter -->
 		<script type="text/javascript">
-					(function(d, w, c) {
-						(w[c] = w[c] || []).push(function() {
-							try {
-								w.yaCounter24484718 = new Ya.Metrika({id: 24484718,
-									clickmap: true,
-									trackLinks: true,
-									accurateTrackBounce: true});
-							} catch (e) {
-							}
-						});
+			(function(d, w, c) {
+				(w[c] = w[c] || []).push(function() {
+					try {
+						w.yaCounter24484718 = new Ya.Metrika({id: 24484718,
+							webvisor: true,
+							clickmap: true,
+							trackLinks: true,
+							accurateTrackBounce: true});
+					} catch (e) {
+					}
+				});
 
-						var n = d.getElementsByTagName("script")[0],
-								s = d.createElement("script"),
-								f = function() {
-									n.parentNode.insertBefore(s, n);
-								};
-						s.type = "text/javascript";
-						s.async = true;
-						s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+				var n = d.getElementsByTagName("script")[0],
+						s = d.createElement("script"),
+						f = function() {
+							n.parentNode.insertBefore(s, n);
+						};
+				s.type = "text/javascript";
+				s.async = true;
+				s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
 
-						if (w.opera == "[object Opera]") {
-							d.addEventListener("DOMContentLoaded", f, false);
-						} else {
-							f();
-						}
-					})(document, window, "yandex_metrika_callbacks");
+				if (w.opera == "[object Opera]") {
+					d.addEventListener("DOMContentLoaded", f, false);
+				} else {
+					f();
+				}
+			})(document, window, "yandex_metrika_callbacks");
 		</script>
 		<noscript><div><img src="//mc.yandex.ru/watch/24484718" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 		<!-- /Yandex.Metrika counter -->
